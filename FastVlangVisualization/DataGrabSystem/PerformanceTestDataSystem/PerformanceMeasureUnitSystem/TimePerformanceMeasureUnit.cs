b7c4c3c45b17a4f46e95474b66c90544009f57e9
Artem Yurchenko <zumikq78@gmail.com>
2021-12-11 12:56:51 +0100

    TimePerformanceMeasureUnit - ConvertRawValueToNumerical method was implemetned

--- FastVlangVisualization/DataGrabSystem/PerformanceTestDataSystem/PerformanceMeasureUnitSystem/TimePerformanceMeasureUnit.cs
+++ FastVlangVisualization/DataGrabSystem/PerformanceTestDataSystem/PerformanceMeasureUnitSystem/TimePerformanceMeasureUnit.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace FastVlangVisualization.DataGrabSystem.PerformanceTestDataSystem.PerformanceMeasureUnitSystem;
 
 public class TimePerformanceMeasureUnit : BasePerformanceMeasureUnit
@@ -6,6 +8,11 @@ public class TimePerformanceMeasureUnit : BasePerformanceMeasureUnit
 	
 	protected override int ConvertRawValueToNumerical ()
 	{
+		if (RawValue.Contains("ms", StringComparison.OrdinalIgnoreCase) == true)
+		{
+			return int.Parse(new Regex("[0-9]*").Match(RawValue).Value);
+		}
+		
 		return 0;
 	}
 }
