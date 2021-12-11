0bbd08207beb5da8e1234061198bb5340520b5af
Artem Yurchenko <zumikq78@gmail.com>
2021-12-11 17:52:09 +0100

    FastVlangWebPageDataGrabber class was fully refactored

--- FastVlangVisualization/DataGrabSystem/FastVlangWebPageDataGrabber.cs
+++ FastVlangVisualization/DataGrabSystem/FastVlangWebPageDataGrabber.cs
@@ -8,6 +8,13 @@ public class FastVlangWebPageDataGrabber : IDataGrabber
 {
 	private string FastVlangWebPageAddress { get; }
 
+	private const string TABLE_CELL_HTML_ATTRIBUTE = "td";
+	private const string TABLE_ROW_HTML_ATTRIBUTE = "tr";
+	private const int EXPECTED_SERVICE_DATA_CELLS_COUNT = 3;
+	
+	private static readonly string[] TIME_RESULTS_MARKERS = {"ms"};
+	private static readonly string[] MEMORY_RESULTS_MARKERS = {"KB"};
+
 	public FastVlangWebPageDataGrabber (string fastVlangWebPageAddress)
 	{
 		FastVlangWebPageAddress = fastVlangWebPageAddress;
@@ -16,6 +23,7 @@ public class FastVlangWebPageDataGrabber : IDataGrabber
 	public async Task<List<IVlangPerformanceTestData>> GetVlangSpeedDataAsync ()
 	{
 		string webPageContent = await GetWebPageContent();
+
 		return ParseWebPageContent(webPageContent);
 	}
 
@@ -28,13 +36,53 @@ public class FastVlangWebPageDataGrabber : IDataGrabber
 
 	private List<IVlangPerformanceTestData> ParseWebPageContent (string webPageContent)
 	{
+		HtmlNode[] testResultsTableRows = GetPerformanceResultsTableRows(webPageContent);
+		string[] tableHeaders = GetTableHeaders(testResultsTableRows[0]);
+
+		return GetPerformanceDataCollection(testResultsTableRows, tableHeaders);
+	}
+
+	private List<IVlangPerformanceTestData> GetPerformanceDataCollection (IReadOnlyList<HtmlNode> testResultsTableRows, IReadOnlyList<string> tableHeaders)
+	{
+		List<IVlangPerformanceTestData> vlangPerformanceTestDataCollection = new();
+		int expectedTestsCount = tableHeaders.Count - EXPECTED_SERVICE_DATA_CELLS_COUNT;
+		List<VlangPerformanceTestData> vlangPerformanceTestDataBuffer = new(expectedTestsCount);
+
+		for (int rowIndex = 1; rowIndex < testResultsTableRows.Count; rowIndex++)
+		{
+			HtmlNode[] tableCellsNodes = GetNextTableRowCells(testResultsTableRows[rowIndex]);
+			PreparePerformanceTestDataBuffer(tableCellsNodes);
+			InsertPerformanceResults(tableCellsNodes, vlangPerformanceTestDataBuffer);
+			vlangPerformanceTestDataCollection.AddRange(vlangPerformanceTestDataBuffer);
+		}
+
+		return vlangPerformanceTestDataCollection;
+
+		void PreparePerformanceTestDataBuffer (IReadOnlyList<HtmlNode> tableCellsNodes)
+		{
+			vlangPerformanceTestDataBuffer.Clear();
+
+			for (int dataIndex = 0; dataIndex < expectedTestsCount; dataIndex++)
+			{
+				VlangPerformanceTestData newData = new();
+				newData.SetTestName(tableHeaders[dataIndex + EXPECTED_SERVICE_DATA_CELLS_COUNT]);
+				InsertServiceData(newData, tableCellsNodes);
+				vlangPerformanceTestDataBuffer.Add(newData);
+			}
+		}
+	}
+
+	private HtmlNode[] GetPerformanceResultsTableRows (string webPageContent)
+	{
 		HtmlDocument htmlDocument = new();
 		htmlDocument.LoadHtml(webPageContent);
 
-		using IEnumerator<HtmlNode> testResultsTableCells = htmlDocument.DocumentNode.Descendants("tr").GetEnumerator();
+		return htmlDocument.DocumentNode.Descendants(TABLE_ROW_HTML_ATTRIBUTE).ToArray();
+	}
 
-		testResultsTableCells.MoveNext();
-		HtmlNode[] tableHeaderNodes = testResultsTableCells.Current.ChildNodes.Where(node => node.Name == "td").ToArray();
+	private string[] GetTableHeaders (HtmlNode testResultsTableRow)
+	{
+		HtmlNode[] tableHeaderNodes = GetNextTableRowCells(testResultsTableRow);
 		string[] tableHeaders = new string[tableHeaderNodes.Length];
 
 		for (int headerIndex = 0; headerIndex < tableHeaderNodes.Length; headerIndex++)
@@ -42,72 +90,61 @@ public class FastVlangWebPageDataGrabber : IDataGrabber
 			tableHeaders[headerIndex] = tableHeaderNodes[headerIndex].InnerText;
 		}
 
-		List<IVlangPerformanceTestData> data = new();
-		const int expectedTestsResultsCellsCount = 11;
-		List<VlangPerformanceTestData> testsDataBuffer = new(expectedTestsResultsCellsCount);
+		return tableHeaders;
+	}
 
-		while (testResultsTableCells.MoveNext() == true)
+	private HtmlNode[] GetNextTableRowCells (HtmlNode testResultsTableRow)
+	{
+		return testResultsTableRow.ChildNodes.Where(node => node.Name == TABLE_CELL_HTML_ATTRIBUTE).ToArray();
+	}
+
+	private void InsertServiceData (VlangPerformanceTestData newData, IReadOnlyList<HtmlNode> tableCellsNodes)
+	{
+		newData.SetTimestamp(DateTime.Parse(tableCellsNodes[0].InnerText));
+		newData.SetCommitID(tableCellsNodes[1].InnerText);
+		newData.SetCommitMessage(tableCellsNodes[2].InnerText);
+	}
+
+	private void InsertPerformanceResults (IReadOnlyList<HtmlNode> tableCellsNodes, IReadOnlyList<VlangPerformanceTestData> vlangPerformanceTestDataBuffer)
+	{
+		for (int cellIndex = EXPECTED_SERVICE_DATA_CELLS_COUNT; cellIndex < tableCellsNodes.Count; cellIndex++)
 		{
-			testsDataBuffer.Clear();
+			string cellText = tableCellsNodes[cellIndex].InnerText;
+			IPerformanceMeasureUnit resultsUnit = CreateSuitablePerformanceResultsUnit(cellText);
+			vlangPerformanceTestDataBuffer[cellIndex - EXPECTED_SERVICE_DATA_CELLS_COUNT].SetPerformanceResult(resultsUnit);
+		}
+	}
 
-			for (int testIndex = 0; testIndex < expectedTestsResultsCellsCount; testIndex++)
-			{
-				VlangPerformanceTestData newData = new();
-				newData.SetTestName(tableHeaders[testIndex + 3]);
-				testsDataBuffer.Add(newData);
-			}
+	private IPerformanceMeasureUnit CreateSuitablePerformanceResultsUnit (string cellText)
+	{
+		IPerformanceMeasureUnit resultsUnit;
 
-			List<HtmlNode> test = testResultsTableCells.Current.ChildNodes.Where(node => node.Name == "td").ToList();
-
-			for (int cellIndex = 0; cellIndex < test.Count; cellIndex++)
-			{
-				string cellText = test[cellIndex].InnerText;
-
-				if (cellIndex == 0)
-				{
-					for (int testIndex = 0; testIndex < expectedTestsResultsCellsCount; testIndex++)
-					{
-						testsDataBuffer[testIndex].SetTimestamp(DateTime.Parse(cellText));
-					}
-				}
-				else if (cellIndex == 1)
-				{
-					for (int testIndex = 0; testIndex < expectedTestsResultsCellsCount; testIndex++)
-					{
-						testsDataBuffer[testIndex].SetCommitID(cellText);
-					}
-				}
-				else if (cellIndex == 2)
-				{
-					for (int testIndex = 0; testIndex < expectedTestsResultsCellsCount; testIndex++)
-					{
-						testsDataBuffer[testIndex].SetCommitMessage(cellText);
-					}
-				}
-				else if (cellIndex < expectedTestsResultsCellsCount)
-				{
-					IPerformanceMeasureUnit resultsUnit;
-
-					if (cellText.Contains("ms"))
-					{
-						resultsUnit = new TimePerformanceMeasureUnit(cellText);
-					}
-					else if (cellText.Contains("KB"))
-					{
-						resultsUnit = new MemoryPerformanceMeasureUnit(cellText);
-					}
-					else
-					{
-						resultsUnit = new LinesPerformanceMeasureUnit(cellText);
-					}
-
-					testsDataBuffer[cellIndex - 3].SetPerformanceResult(resultsUnit);
-				}
-			}
-
-			data.AddRange(testsDataBuffer);
+		if (CheckIfResultContainsAnyMarker(TIME_RESULTS_MARKERS, cellText) == true)
+		{
+			resultsUnit = new TimePerformanceMeasureUnit(cellText);
+		}
+		else if (CheckIfResultContainsAnyMarker(MEMORY_RESULTS_MARKERS, cellText) == true)
+		{
+			resultsUnit = new MemoryPerformanceMeasureUnit(cellText);
+		}
+		else
+		{
+			resultsUnit = new LinesPerformanceMeasureUnit(cellText);
 		}
 
-		return data;
+		return resultsUnit;
+	}
+
+	private bool CheckIfResultContainsAnyMarker (IReadOnlyList<string> markers, string result)
+	{
+		for (int markerIndex = 0; markerIndex < markers.Count; markerIndex++)
+		{
+			if (result.Contains(markers[markerIndex], StringComparison.OrdinalIgnoreCase) == true)
+			{
+				return true;
+			}
+		}
+
+		return false;
 	}
 }
