0e3d18f8d85528dc069d09e69808104ad751a2f4
Artem Yurchenko <zumikq78@gmail.com>
2021-12-11 12:25:20 +0100

    Tests project was created; PerformanceTestDataSystemTests class was created with a template test

---
+++ Tests/DataGrabSystemTests/PerformanceTestDataSystemTests.cs
@@ -0,0 +1,17 @@
+using FastVlangVisualization.DataGrabSystem.PerformanceTestDataSystem.PerformanceMeasureUnitSystem;
+using NUnit.Framework;
+
+namespace Tests.DataGrabSystemTests;
+
+public class PerformanceTestDataSystemTests
+{
+	[Test]
+	public void CreateTimePerformanceUnit_GetNumericalValueReturnsExpectedValue_True ()
+	{
+		int testNumber = 256;
+		string testRawValue = $"{testNumber} ms";
+		IPerformanceMeasureUnit testPerformanceUnit = new TimePerformanceMeasureUnit(testRawValue);
+		
+		Assert.IsTrue(testNumber == testPerformanceUnit.GetNumericalValue());
+	}
+}
