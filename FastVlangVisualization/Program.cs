bdb8582b290379db97bb7b7c4dd463c97a9b1c89
Artem Yurchenko <zumikq78@gmail.com>
2021-12-13 20:11:06 +0100

    Radzen DialogueService was set up

--- FastVlangVisualization/Program.cs
+++ FastVlangVisualization/Program.cs
@@ -2,6 +2,7 @@ using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using FastVlangVisualization;
+using Radzen;
 
 WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped<DialogService>();
 
 await builder.Build().RunAsync();
