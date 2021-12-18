7e72f4397601c4defb0f03c8e107b7a37160598c
Artem Yurchenko <zumikq78@gmail.com>
2021-12-18 23:27:29 +0100

    Advanced loading status system was implemented

---
+++ FastVlangVisualization/ProgressSystem/VlangBenchmarksPlotsLoadingStatus.cs
@@ -0,0 +1,9 @@
+namespace FastVlangVisualization.ProgressSystem;
+
+public enum VlangBenchmarksPlotsLoadingStatus
+{
+	PARSING_TEST_RESULTS,
+	PROCESSING_TEST_RESULTS,
+	PREPARING_PLOTS_DATA,
+	READY
+}
